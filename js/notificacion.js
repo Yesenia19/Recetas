54e118db7c0fb8908b319a688cfd00cc0a0ed782
Yesenia <1719110284@utectulancingo.edu.mx>
2023-03-23 11:37:19 -0600

    v1

--- js/notificacion.js
+++ js/notificacion.js
@@ -4,7 +4,7 @@ Notification.requestPermission().then(permission => {
       // El usuario ha permitido las notificaciones
         const notification = new Notification('Nuevo mensaje', {
         body: 'Tienes un nuevo mensaje',
-        badge: 'https://yesenia19.github.io/Recetas/moneda.png'
+        badge: 'https://github.com/Yesenia19/Recetas/blob/61843857a1b93c0148068b4aa53a3b1f9506630e/images/moneda.png'
     });
     } else {
         console.log("El usuario ha denegado las notificaciones");
