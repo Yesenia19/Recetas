38a844ddb74288f1c82debddc571ed20ea49c0bf
Yesenia <1719110284@utectulancingo.edu.mx>
2023-03-29 13:04:13 -0600

    diseño

--- js/notificacion.js
+++ js/notificacion.js
@@ -1,26 +1,28 @@
+
 Notification.requestPermission().then(permission => {
-    if (permission === 'granted') {
-        console.log("El usuario ha permitido las notificaciones");
-      // El usuario ha permitido las notificaciones
-        new Notification('Nuevo mensaje', {
+  if (permission === 'granted') {
+    console.log("El usuario ha permitido las notificaciones");
+    // El usuario ha permitido las notificaciones
+    new Notification('Nuevo mensaje', {
         body: 'Revisa las actualizaciones que tenemos para ti',
-        icon: 'images/icon-384x384.png',
-        badge: 'images/badge.png'
+        icon: 'images/icon-384x384.png'
     }); 
-    } else {
-        console.log("El usuario ha denegado las notificaciones");
+  } else {
+      console.log("El usuario ha denegado las notificaciones");
       // El usuario ha denegado las notificaciones
     }
   });
+  
+
 
 //To display an empty badge
 //navigator.setAppBadge();
 
 //To display a number in the badge
-navigator.setAppBadge(42);
+//navigator.setAppBadge(42);
 
-navigator.setAppBadge(42).then(() => {
+/*navigator.setAppBadge(42).then(() => {
     console.log("La badge fue agregada");
 }).catch(e => {
     console.error("Error displaying the badge", e);
-});
+});*/
--- serviceWorker.js
+++ serviceWorker.js
@@ -3,20 +3,9 @@ const staticMonedas = "Recetas-site-v1"
 const assets = [
     "/",
     "/index.html",
-    "/css/style.css",
+    "/css/styles.css",
     "/js/app.js",
-    "/images/20_Const1917_rev_pagMon20.png",
-    "/images/C_20pesosBelisario_144.png",
-    "/images/C_20pesosEjercito_144.png",
-    "/images/C_20pesosPazCambioMilenio_144.png",
-    "/images/C_20pesosPazNobel_144.png",
-    "/images/C_20pesosVeracruz_144.png",
-    "/images/C_20pesosZacatecas_142.png",
-    "/images/C_mon20pesos_PlanMarina.png",
-    "/images/Fuerza_Aerea_20_revTransp_peq.png",
-    "/images/Morelos_20_anvTransp_peq.png",
-    "/images/C_mon_20_pesos_PlanDNIIIE.png",
-    "images/icono.png",
+    "/images/icono.png",
 ]
 
 self.addEventListener("install", installEvent => {
@@ -34,4 +23,3 @@ self.addEventListener("fetch", fetchEvent => {
         })
     )
 })
-
