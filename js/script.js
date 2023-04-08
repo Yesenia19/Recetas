a4f92bfcf8d82c575877ac4780f877ebb535707a
Yesenia <1719110284@utectulancingo.edu.mx>
2023-04-07 21:45:38 -0600

    menu

--- js/script.js
+++ js/script.js
@@ -1,41 +1,46 @@
-const navToggle = document.querySelector(".nav-toggle");
-const navMenu = document.querySelector(".nav-menu");
+// Optenemos open-close
+const $openClose = document.getElementById("open-close"),
+      // Optenemos el atributo id de nuetro aside
+      $aside = document.getElementById("aside");
 
-navToggle.addEventListener("click", () => {
-    navMenu.classList.toggle("nav-menu_visible");
-    
-    if (navMenu.classList.contains("nav-menu_visible")) {
-            navToggle.setAttribute("aria-label", "Cerrar menú");
-    } else {
-        navToggle.setAttribute("aria-label", "Abrir-menú");
-    }
-});
-const menuLinks = document.querySelectorAll('.nav-menu a[href^="#"]');
-
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach(entry => {
-        const id = entry.target.getAttribute("id");
-        const menuLink = document.querySelector(`.nav-menu a[href="#${id}"]`)
-
-        if (entry.isIntersecting){
-            menuLink.classList.add("nav-menu-link_active");
-        }
-        else {
-            menuLink.classList.remove("nav-menu-link_active")
-        }
-    });
-}, {rootMargin: "-30% 0px -70% 0px"});
-
-menuLinks.forEach(menuLink => {
-    menuLink.addEventListener("click", function() {
-        navMenu.classList.remove("nav-menu_visible");
-    })
-
-    const hash = menuLink.getAttribute("href");
-    const target = document.querySelector(hash);
-    if (target){
-        observer.observe(target);
-    }
+ // Damos funcionalidad al menú
+$openClose.addEventListener("click",()=>{
+    // Acedemos a la variable aside y hacemos que despliege y se oculte 
+    $aside.classList.toggle("desplegar")
 })
 
+// Submenus
+let listElements = document.querySelectorAll('.list__button--click');
 
+listElements.forEach(listElement => {
+    listElement.addEventListener('click', ()=>{
+        
+        listElement.classList.toggle('arrow');
+
+        let height = 0;
+        let menu = listElement.nextElementSibling;
+        if(menu.clientHeight == "0"){
+            height=menu.scrollHeight;
+        }
+
+        menu.style.height = `${height}px`;
+
+    })
+});
+
+// Notificacions
+var box  = document.getElementById('box');
+var down = false;
+
+
+function toggleNotifi(){
+	if (down) {
+		box.style.height  = '0px';
+		box.style.opacity = 0;
+		down = false;
+	}else {
+		box.style.height  = '510px';
+		box.style.opacity = 1;
+		down = true;
+	}
+}
