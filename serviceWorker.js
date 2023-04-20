ef10ef42655af120a695bf2d793200b83490accb
Yesenia <1719110284@utectulancingo.edu.mx>
2023-04-20 05:07:27 -0600

    serviceWorker

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,16 +1,24 @@
-const staticMonedas = "Recetas-site-v1"
+const StaticRecetas = "Recetas-site-v1"
 
 const assets = [
     "/",
     "/index.html",
     "/css/styles.css",
+    "/css/contacto.css",
+    "/css/inicio.css",
     "/js/app.js",
-    "/images/icono.png",
+    "/images/likibhbjn.png",
+    "/bebidas.html",
+    "/contacto.html",
+    "/destacadas.html",
+    "/platos-principales.html",
+    "/postres.html"
+    
 ]
 
 self.addEventListener("install", installEvent => {
     installEvent.waitUntil(
-        caches.open(staticMonedas).then(cache => {
+        caches.open(StaticRecetas).then(cache => {
             cache.addAll(assets)
         })
     )
